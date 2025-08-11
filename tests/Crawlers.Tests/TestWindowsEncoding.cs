46c08a8aa91c4c65c9885fe798c95aa6e1de53d2
Rock de Vocht <rock@simsage.ai>
2025-08-11 10:35:55 +0100

    implemented windows 1252 encoding and unit test

--- tests/Crawlers.Tests/TestWindowsEncoding.cs
+++ tests/Crawlers.Tests/TestWindowsEncoding.cs
@@ -4,12 +4,13 @@ namespace Crawlers.Tests;
 
 public class TestWindowsEncoding
 {
-    // check we encode a windows 1252 string to a utf-8 string on windows
+    // check we encode a Windows 1252 string to a utf-8 string on windows
     [Fact]
     public void EncodingTest1()
     {
         if (RockUtils.IsWindows())
         {
+            RockUtils.RegisterEncodingProviders();
             byte[] windows1252Bytes = [0x66, 0x72, 0x61, 0x6e, 0xe7, 0x61, 0x69, 0x73, 0x20, 0xa9];
             var windowsStr = Encoding.GetEncoding("Windows-1252").GetString(windows1252Bytes);
             var utf8Str = RockUtils.Windows1252ToUtf8(windowsStr);
